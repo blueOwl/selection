cb2c3ff1d854b2cef30049d354f53433016391fc
Zhu Liu <zhuliu@Zhus-MacBook-Pro.local>
2018-12-04 15:42:04 -0800

    add child pos dis and local env volume

--- model.py
+++ model.py
@@ -16,11 +16,21 @@ MAX_GEN_TICK = 5000
 ENV_PRESS_PERIOD = 100
 MAX_CAPACITY = 1000
 IMPECT_R = 6
+ENV_R = 10
 ENV_STRESS_COF = 1
 MUTATION_VAR = 0.05
 
 T_MAX, T_MIN, T_OPT = 1, -2, 0
 ALPHA = math.log(2) / math.log((T_MAX - T_MIN) / (T_OPT - T_MIN))
+
+#global var control env growth rate
+VERT_MAX = 200
+HORZ_MAX = 200
+ALPHA12 = 0.5
+ALPHA21 = 0.5
+#child position distribution
+POS_DIS = [2,1]
+#(.15, .3, .3, .15)
 
 def beta(T, tp):
     #if T < 0: T = -1 * T
@@ -73,9 +83,9 @@ class GenModel(Model):
 
         #model para
         self.r1, self.r2 = 1.0, 1.0
-        self.her_max = MAX_CAPACITY
-        self.ver_max = MAX_CAPACITY
-        self.alpha12, self.alpha21 = 0.5, 0.5
+        #self.her_max = MAX_CAPACITY
+        #self.ver_max = MAX_CAPACITY
+        #self.alpha12, self.alpha21 = 0.5, 0.5
         self.env_press = (np.sin(np.linspace(0, np.pi * 2 * ENV_PRESS_PERIOD, MAX_GEN_TICK)) + 1 ) / 2 * ENV_STRESS_COF
         #this is how enviroment values generated
         #all values are chosen from a rescaled sin functions
@@ -120,19 +130,19 @@ class GenModel(Model):
     def get_popu_size(self):
         return self.schedule.get_agent_count()
 
-    def get_gr_rate(self):
-        cur_popu = self.get_popu_size()
-        her_popu = self.get_her_popu_size()
-        ver_popu = self.get_ver_popu_size()
-        #r1, r2 = self.get_r()
-        #ver_rate = r1 * (self.ver_max - ver_popu - self.alpha21 * her_popu)/self.ver_max
-        #her_rate = r2 * (self.her_max - her_popu - self.alpha12 * ver_popu)/self.her_max
-        ver_rate = (self.ver_max - ver_popu - self.alpha21 * her_popu)/self.ver_max
-        her_rate = (self.her_max - her_popu - self.alpha12 * ver_popu)/self.her_max
-        print("popu and rate:")
-        print(ver_popu, ver_rate)
-        print(her_popu, her_rate)
-        return (ver_rate, her_rate)
+#    def get_gr_rate(self):
+#        cur_popu = self.get_popu_size()
+#        her_popu = self.get_her_popu_size()
+#        ver_popu = self.get_ver_popu_size()
+#        #r1, r2 = self.get_r()
+#        #ver_rate = r1 * (self.ver_max - ver_popu - self.alpha21 * her_popu)/self.ver_max
+#        #her_rate = r2 * (self.her_max - her_popu - self.alpha12 * ver_popu)/self.her_max
+#        ver_rate = (self.ver_max - ver_popu - self.alpha21 * her_popu)/self.ver_max
+#        her_rate = (self.her_max - her_popu - self.alpha12 * ver_popu)/self.her_max
+#        #print("popu and rate:")
+#        #print(ver_popu, ver_rate)
+#        #print(her_popu, her_rate)
+#        return (ver_rate, her_rate)
 
 
 
@@ -148,12 +158,11 @@ class GenModel(Model):
         self.datacollector.collect(self)
         print("population size: ", self.get_popu_size())
         print("env pressure", self.cur_press)
-        self.gr_rate = self.get_gr_rate()
+        #self.gr_rate = self.get_gr_rate()
         if self.press:print("env die")
         self.schedule.step()
 
 class GenAgent(Agent):
-    """ An agent with fixed initial wealth."""
     def __init__(self, unique_id, model, generation_num, gen_type = None, gen_info = None):
         super().__init__(unique_id, model)
         self.generation_num = generation_num
@@ -170,9 +179,6 @@ class GenAgent(Agent):
         self.model.grid.remove_agent(self)
         self.model.schedule.remove(self)
 
-    def check_life(self):
-        if self.lifetime <= 0:
-            self.die()
     @staticmethod
     def mutate_gen_info(info):
         mu = np.array(list(map(range_filter(-1 * 2 * MUTATION_VAR, 2 * MUTATION_VAR), np.random.normal(0, MUTATION_VAR, MUT_GEN_LENGHT))))
@@ -189,10 +195,34 @@ class GenAgent(Agent):
         info = np.random.choice(neighbour_info, size=len(self.gen_info),  replace=True)
         return self.mutate_gen_info(info)
 
+    def get_local_env_volume_gr_rate(self):
+        neighbours = [agent.gen_type for agent in self.model.grid.get_neighbors(self.pos, True, include_center=False, radius=ENV_R)]
+        vert_popu, horz_popu = len(neighbours), np.sum(neighbours)
+        vert_rate = (VERT_MAX - vert_popu - ALPHA21 * horz_popu)/VERT_MAX
+        horz_rate = (HORZ_MAX - horz_popu - ALPHA12 * vert_popu)/HORZ_MAX
+        return (vert_rate, horz_rate)
+
+
+    @staticmethod
+    def get_pos_dis(l, dis):
+        #return a prob list of position distribution
+        #the length position list is l and should be even(without center)
+        #pos near by center has less dis idx
+        partition = len(dis) 
+        prob = []
+        for i in range(1, l // 2 + 1):
+            idx = math.ceil((i * 1.0 / (l//2) * partition)) - 1
+            prob.append(dis[idx])
+        re_prob = sorted(prob[:], reverse = True)
+        probs = re_prob + prob
+        return np.array(probs) / (1.0 * sum(probs))
 
     def gener_sus(self):
+	#agent make a child
         next_gen_num = self.generation_num + 1
-        pos = random.choice(self.model.grid.get_neighborhood(self.pos, True, include_center=False, radius=IMPECT_R))
+        neighborhood = self.model.grid.get_neighborhood(self.pos, True, include_center=False, radius=IMPECT_R)
+        pos_idx = np.random.choice(range(len(neighborhood)), 1, p=self.get_pos_dis(len(neighborhood), POS_DIS), replace=False)
+        pos = neighborhood[int(pos_idx)]
         if self.gen_type == 0:
             gen_info = self.get_gen_info_vetical()
         else:
@@ -202,10 +232,13 @@ class GenAgent(Agent):
         self.model.grid.place_agent(sus, pos)
 
     def get_gener_p(self):
-        uni_gr_rate = self.model.gr_rate[self.gen_type]
+	#total growth prob
+	#enviroment size growth rate * genetic infor coef
+        env_gr_rate = self.get_local_env_volume_gr_rate()[self.gen_type]
         #self_gen_cof = 0.5 + two_curve(np.mean(self.gen_info)) * 0.5
         self_gen_cof = beta(np.mean(self.gen_info) - self.model.cur_press, self.gen_type)
-        return uni_gr_rate * self_gen_cof
+        #return uni_gr_rate * self_gen_cof
+        return env_gr_rate * self_gen_cof
 
     def check_env_press(self):
         press = self.model.cur_press
@@ -213,16 +246,28 @@ class GenAgent(Agent):
         if press - np.mean(self.gen_info)  > coin:
             self.lifetime = 0
 
+    def check_life(self):
+        #check whether agent is alive
+        #delete angent if it is dead
+        #return true if it is alive
+        if self.lifetime <= 0:
+            self.die()
+            return False
+        return True
+	
     def step(self):
+        #agent behavior 
+        # 1. check whether still alive
+        # 2. get generate child prob(self.get_gener_p) and decide whether generate child(self.gener_sus)
         self.lifetime -= 1
-        coin = np.random.random()
-        self.p = self.get_gener_p()
-        #print(coin, p)
-        if coin < self.p:
-            self.gener_sus()
         if self.model.press:
             self.check_env_press()
-        self.check_life()
+        alive = self.check_life()
+        if alive:
+            coin = np.random.random()
+            self.p = self.get_gener_p()
+            if coin < self.p:
+                self.gener_sus()
 
 
 '''
